1d5755e9a8132375a4e3395276a602d9e6449bc3
kasramoh030 <kasramohammadi152@gmail.com>
2026-09-18 18:32:03 +0330

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-print("Beauty AI Agent started")
+from agent import ask_agent
+
+
+print("Beauty AI Agent Started")
+
+while True:
+
+    message = input("\nمشتری: ")
+
+    if message == "exit":
+        break
+
+    answer = ask_agent(message)
+
+    print("\nAgent:")
+    print(answer)
